f3536bcc937b2c80b7fb51fd638a3eee8edf44fe
Waqar Ahmad Khan <waqarbinmuhammad@gmail.com>
2022-06-18 14:47:33 +0500

    Create hello.py

---
+++ Day1/hello.py
@@ -0,0 +1,13 @@
+# Print, String, Integer, Float, Input, Type Casting
+
+print("What is your name?")
+myName = input() #Enter String
+
+print("What is your age?")
+myAge = input() #Enter Integer
+
+print("What is your score in last exam?")
+myScore = input() #Enter Float
+
+print('\nHello,' + myName + '! You will be ' + str(int(myAge)+1) + ' in a year.')
+print("Your score in last exam was "+ str(myScore))
