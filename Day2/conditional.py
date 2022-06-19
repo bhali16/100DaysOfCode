3b85d899c1b0ba065a8e34647f68425908003396
Waqar Ahmad Khan <waqarbinmuhammad@gmail.com>
2022-06-19 17:35:41 +0500

    Create conditional.py

---
+++ Day2/conditional.py
@@ -0,0 +1,9 @@
+# Can I drive a Car
+
+print('Enter the age of the driver') # Ask for the age of the driver
+age = int(input())
+
+if(age >= 16): # If the driver is 16 or older
+    print('You can drive')
+else:
+    print('You cannot drive')
